c7731d9d8585ec499ada1f7ef77f86f4bedfade7
hotsuma kawashima <hkawashimajob1992@gmail.com>
2025-01-22 10:55:13 +0900

    update

--- js/form-assist.js
+++ js/form-assist.js
@@ -1,5 +1,18 @@
 console.log('form-assist.js loaded');
 
+const triggerAttributeEvent = (element) => {
+    console.log('triggerAttributeEvent is called');
+
+    const events = ['onclick', 'onchange', 'onblur']
+    let i, len;
+
+    for ( i = 0, len = events[length]; i < len; i++ ) {
+      if ( element[events[i]] ) {
+        element[events[i]].call(element);
+      }
+    }
+}
+
 setTimeout(() => {
   const name = '名前';
 
@@ -7,6 +20,11 @@ setTimeout(() => {
   input.value = name;
 
   console.log({ location: window.location.href });
+
+  triggerAttributeEvent(input);
+
+  //let event = new Event('change');
+  //input.dispatchEvent(event);
 }, 100);
 
 
--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+
   return (
     <form action="" method="post">
       <div>
         <label>お名前:</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+            console.log('setName in onChange')
+          }} />
       </div>
     </form>
   );
